f63e6f2551ec40092eb772e3b1ae5802722b2ebf
Shima-Oh <shimasound04@gmail.com>
2021-12-17 04:12:32 +0900

    feat(nextauth.ts): アクセストークンの更新などを追加

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,31 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
 import NextAuth from 'next-auth'
+import { JWT } from 'next-auth/jwt';
 import SpotifyProvider from 'next-auth/providers/spotify'
 import { authorizationUrl } from '../../../lib/authorizationUrl';
+import spotifyApi from '../../../lib/spotifyApi';
+
+const refreshAccessToken = async (token: JWT) => {
+  try {
+    spotifyApi.setAccessToken(token.accessToke as string);
+    spotifyApi.setRefreshToken(token.refreshToken as string);
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken()
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now() + refreshedToken.expires_in * 1000,
+      refreshedToken: refreshedToken.refresh_token ?? token.refreshToken
+    }
+  } catch (error) {
+    console.log(error)
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError"
+    }
+  }
+}
 
 export default NextAuth({
   providers: [
@@ -18,11 +42,19 @@ export default NextAuth({
         return {
           ...token,
           accessToken: account.access_token,
+          accessTokenExpires: Date.now() + Number(account.expires_at) * 1000,
           refreshToken: account.refresh_token,
-        };
+        }
       }
 
-      return token
+      // アクセストークンの期限が切れていなければトークンをそのまま返す
+      if (Date.now() < token.accessTokenExpires) {
+        return token
+      }
+
+      // トークンの期限が切れているなら更新する
+      return refreshAccessToken(token)
+
     },
 
     async session({ session, token }) {
